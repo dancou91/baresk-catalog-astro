731b7456f705dc619fb114fb3bf7e67ff06a7ab0
dancou91 <dan_cou91@hotmail.com>
2024-10-26 01:58:08 +0200

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
     integrations: [tailwind()],
-    site: 'https://dancou91.github.io'
+    site: 'https://dancou91.github.io',
+    base: 'baresk-catalog-astro',
 });
